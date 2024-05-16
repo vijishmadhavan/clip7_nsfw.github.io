f314315bb37df82756a3aa0a258c1ccde53a2e7b
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2024-05-16 17:39:48 +0530

    Update and rename nsfwDetectorClass2.js to nsfwDetectorClass.js

--- nsfwDetectorClass.js
+++ nsfwDetectorClass.js
@@ -23,7 +23,7 @@ class NsfwDetector {
     
             // Check if the top class is related to a child
             const topClass = output[0];
-            const isChildRelated = /child|kid|baby|toddler|preschooler|school_age_child|preteen|adolescent|boy|girl/i.test(topClass.label);
+            const isChildRelated = /LINGERIE|naked|nude|child|kid|baby|toddler|preschooler|school_age_child|preteen|adolescent|boy|girl/i.test(topClass.label);
     
             // If the top class is related to a child, mark it as NSFW
             if (isChildRelated) {
