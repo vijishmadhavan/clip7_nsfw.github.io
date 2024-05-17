6de9fb7a371275d942a1239c158bc203da05a3a1
Vijish Madhavan <53169213+vijishmadhavan@users.noreply.github.com>
2024-05-17 11:24:34 +0530

    Update nsfwDetectorClass.js

--- nsfwDetectorClass.js
+++ nsfwDetectorClass.js
@@ -1,44 +1,8 @@
 class NsfwDetector {
     constructor() {
-        this._nsfwLabels = [
-            'ANIME_NAKED','ANIME_PORN','CARTOON_NAKED','CARTOON_PORN','CARTOON_KID','FEMALE_BREAST_EXPOSED','KIDS','CHILD_FACE','CHILDREN','TEENAGER','NAKED','NUDE','PARTIAL_NUDITY','FEMALE_GENITALIA_EXPOSED', 'BUTTOCKS_EXPOSED', 'ANUS_EXPOSED',
-            'MALE_GENITALIA_EXPOSED', 'BLOOD_SHED', 'VIOLENCE', 'GORE', 'PORNOGRAPHY', 'DRUGS', 'ALCOHOL','CHILD_PORN',
-            'CHILD_KISS','CHILD_VULGARITY','INAPROPRIATE_CLOTHING','SENSUAL_KISS','CHILD_PLAYING','LYING_ON_BED','BABY',
-            'TODDLER','PRESCHOOLER','SCHOOL_AGE_CHILD','PRETEEN','ADOLESCENT','MALE_CHILD','FEMALE_CHILD','BOY','GIRL',
-            'REVEALING_CLOTHING','LINGERIE','SWIMWEAR','SHEER_CLOTHING','BIKINI','UNDERWEAR','TIGHT_CLOTHING','PLUNGING_NECKLINE',
-            'GARTER_BELT','FETISH_CLOTHING','LATEX_CLOTHING','FISHNET','MINI_SKIRT','BACKLESS_DRESS','TRANSPARENT_CLOTHING','BODYCON_DRESS', 'LEOTARD', 'STOCKINGS','REVEALING_GYM_WEAR',
-            'REAR_CUTOUT_DRESS','BOOTY_DRESS','HIGH_CUT_REAR_DRESS','EXPOSED_BUTTOCKS_DRESS','WET_REVEALING_CLOTHING', 'WET_SHEER_DRESS', 'CLINGY_WET_DRESS', 'SOAKED_TRANSPARENT_CLOTHING','cleavage_dress',
-            'ACCIDENT', 'CRASH', 'DEADLY_INJURY', 'MURDER', 'RIOTS', 'STREET_CRIME', 'VIOLENT_ATTACKS', 'DEAD_BODIES', 'HUMAN_CORPSES', 'ANIMAL_CARCASS',
-            'ASSAULT', 'ARSON', 'ATTACK', 'AMBULANCE', 'POLICE', 'FIRE_TRUCK', 'PARAMEDICS', 'ANIMAL_CRUELTY', 'ELDER_ABUSE', 'CHILD_ABUSE', 'SPOUSAL_ABUSE',
-            'ARMED_SOLDIERS', 'FIGHTER_JETS', 'TANKS', 'MISSILES', 'DESTROYERS', 'SUBMARINES',
-            'EARTHQUAKE_DEVASTATION', 'FLOOD_DESTRUCTION', 'HURRICANE', 'SNOW_STORM', 'TORNADO', 'TSUNAMI', 'WILDFIRE',
-            'VIOLENT_PROTESTS', 'VIOLENT_SCUFFLES', 'VIOLENT_CLASHES', 'STONE_PELTING', 'MOB_VIOLENCE',
-            'ASSAULT_RIFLES', 'GUNS', 'RIFLES', 'KNIVES', 'AXES', 'SWORDS', 'DEADLY_WEAPONS',
-            'FULL-FRONTAL_NUDITY', 'SEXUAL_ACTS', 'HENTAI', 'ANIME_PORN', 'GENITALIA', 'EXPLICIT_SEXUAL_ACT',
-            'PARTIAL_NUDITY', 'SEXUALLY_SUGGESTIVE', 'TITILLATING', 'PARTIAL_CLOTHING', 'SEXUALLY_REVEALING', 'SEXTING', 'TWERKING', 'LEWD_&_LASCIVIOUS_CONDUCT',
-            'ISIS', 'ALQAEDA', 'TALIBAN', 'HEZBOLLAH', 'ISLAMIC_TERRORISM', 'SUICIDE_BOMBING', 'BOMB_BLAST_ATTACK',
-            'KKK', 'WHITE_SUPREMACISTS', 'WHITE-NATIONALIST', 'ANTI-GOVERNMENT_MILITIA', 'ANTI-LGBT', 'ANTI-FEMINIST', 'ANTI-IMMIGRANT', 'ANTI-SEMITISM',
-            'COCAINE', 'HEROIN', 'SUBSTANCE_ABUSE', 'METHAMPHETAMINE', 'OXYCONTIN', 'FENTANYL', 'DRUG_CARTELS', 'DRUG_ADDICTION', 'E-CIGARETTES', 'VAPING',
-            'PROFANE_LANGUAGE', 'OBSCENE_GESTURE', 'LEWD_CONDUCT', 'GRAPHIC_LANGUAGE',
-            'QANON', 'BLACK_LIVES_MATTER', 'ANTI-VACCINE', 'ANTI-MASK', 'GUN_RIGHTS', 'WOKEISM', 'PROUD_BOYS', 'ALT-RIGHT', 'ANTIFA', 'ISLAMOPHOBIA', 'IDENTITY_POLITICS', 'CULTURAL_APPROPRIATION',
-            'HACKING', 'MALWARE', 'PHISHING', 'IDENTITY_THEFT', 'DATA_BREACH', 'SPYWARE',
+        this._threshold = 0.01;
+        this._nsfwLabels = ['NSFW','SFW'
         ];
-        
-        this._sfwLabels = [
-            'FULLY_CLOTHED', 'CONSERVATIVE_CLOTHING', 'MODEST_CLOTHING', 'FORMAL_ATTIRE', 'BUSINESS_ATTIRE',
-            'CASUAL_ATTIRE', 'LOOSE_FITTING_CLOTHING', 'LONG_SLEEVES', 'TROUSERS', 'DRESSES_BELOW_KNEE',
-            'HIGH_NECKLINE', 'TURTLENECK', 'BUTTON_UP_SHIRT', 'COLLARED_SHIRT', 'T-SHIRT', 'BLOUSE', 'SWEATER',
-            'JACKET', 'COAT', 'JEANS', 'SHORTS_KNEE_LENGTH', 'SKIRT_KNEE_LENGTH', 'DRESS_KNEE_LENGTH', 'SUIT',
-            'UNIFORM', 'RELIGIOUS_CLOTHING', 'TRADITIONAL_CLOTHING', 'SPORTS_JERSEY', 'ATHLETIC_WEAR',
-            'SWEATSHIRT', 'SWEATPANTS', 'PAJAMAS', 'SCRUBS', 'LAB_COAT', 'APRON', 'OVERALLS', 'VEST', 'BOWTIE',
-            'NECKTIE', 'SCARF', 'HAT', 'CAP', 'GLOVES', 'MITTENS', 'SOCKS', 'SHOES', 'BOOTS', 'SANDALS', 'SLIPPERS',
-            'COSTUMES_AND_COSPLAY', 'HISTORICAL_CLOTHING', 'CULTURAL_CLOTHING',
-            'SFW', 'SUITABLE_FOR_ALL_AUDIENCES', 'SAFE_CONTENT', 'GENERAL_AUDIENCES', 'SUITABLE_FOR_ALL_AGES',
-            'CHILD-SAFE_CONTENT', 'CARTOONS', 'SAFE_FOR_CHILDREN', 'BLAND_CONTENT', 'MADE_FOR_KIDS',
-            'RELIGIOUS_RITUAL', 'RELIGIOUS_CEREMONY', 'PRAYER', 'WORSHIP', 'RELIGIOUS_GATHERING', 'SPIRITUAL',
-            'CELEBRITIES', 'NEWS_PERSONALITIES', 'FAMOUS_POLITICIANS', 'SPORTS_STARS', 'MOVIE_STARS', 'MUSIC_STARS',
-        ];
-        
         this._classifierPromise = window.tensorflowPipeline('zero-shot-image-classification', 'Xenova/clip-vit-base-patch32');
     }
 
@@ -47,15 +11,11 @@ class NsfwDetector {
         try {
             blobUrl = await this._loadAndResizeImage(imageUrl);
             const classifier = await this._classifierPromise;
-            const output = await classifier(blobUrl, [...this._nsfwLabels, ...this._sfwLabels]);
-    
-            // Check if the top class is NSFW
-            const topClass = output[0];
-            const isNsfw = this._nsfwLabels.includes(topClass.label);
-    
-            console.log(`Classification for ${imageUrl}:`, isNsfw ? 'NSFW' : 'Safe');
-            console.log('Detailed classification results:', output);
-            return isNsfw;
+            const output = await classifier(blobUrl, this._nsfwLabels);
+            const nsfwDetected = output.some(result => result.score > this._threshold);
+            console.log(`Classification for ${imageUrl}:`, nsfwDetected ? 'NSFW' : 'Safe');
+            console.log('Detailed classification results:', output); // Log detailed results
+            return nsfwDetected;
         } catch (error) {
             console.error('Error during NSFW classification: ', error);
             throw error;
@@ -123,3 +83,4 @@ class NsfwDetector {
 }
 
 window.NsfwDetector = NsfwDetector;
+
